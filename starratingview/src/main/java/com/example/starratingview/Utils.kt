6e119f42895e8cfc89b015860b0fd1b3db9da6fb
= <v.lamnt34@vinid.net>
2019-07-14 16:43:07 +0700

    Add circle animation behind stars

--- starratingview/src/main/java/com/example/starratingview/Utils.kt
+++ starratingview/src/main/java/com/example/starratingview/Utils.kt
@@ -5,6 +5,21 @@ import com.example.starratingview.StarState.Companion.ANIMATING
 import com.example.starratingview.StarState.Companion.CHECKED
 import com.example.starratingview.StarState.Companion.UNCHECKED
 
+class Utils {
+    companion object {
+        fun clamp(value: Float, low: Float, high: Float) =
+            Math.min(Math.max(value, low), high)
+
+        fun mapValueFromRangeToRange(
+            value: Float,
+            fromLow: Float,
+            fromHigh: Float,
+            toLow: Float,
+            toHigh: Float
+        ) = (toLow + ((value - fromLow) / (fromHigh - fromLow) * (toHigh - toLow)))
+    }
+}
+
 @StringDef(value = [CHECKED, UNCHECKED, ANIMATING])
 @Retention(AnnotationRetention.SOURCE)
 annotation class StarState {
